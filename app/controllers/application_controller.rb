c90a1708b51e4d71b0e178a805cc4032ed71d935
Trent Moss <trentmoss05@gmail.com>
2019-03-04 22:17:22 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,4 +26,14 @@ class ApplicationController < Sinatra::Base
     @article = Article.create(params)
     redirect to "/articles/#{@article.id}"
   end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
 end
